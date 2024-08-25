f0484abeb2da633054f62593355f6cf5d7b55706
frontendfm <137447016+frontendfaris@users.noreply.github.com>
2024-08-25 12:13:19 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+var typed = new Typed("#skills", {
+  strings: [
+    "build good websites.",
+    "want to work with YOU.",
+    "am eager to learn more.",
+  ],
+  typeSpeed: 90,
+  backSpeed: 50,
+  backDelay: 1000,
+  loop: true,
+});
